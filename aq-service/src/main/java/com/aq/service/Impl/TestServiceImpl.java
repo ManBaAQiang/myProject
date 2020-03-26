b1bd86a98121131511564c93f3f8ee5a1c4a8e48
ManBaAQiang <1216617578@qq.com>
2020-03-26 23:07:18 +0800

    test

---
+++ aq-service/src/main/java/com/aq/service/Impl/TestServiceImpl.java
@@ -0,0 +1,14 @@
+package com.aq.service.Impl;
+
+import com.aq.service.TestService;
+
+/**
+ * @ClassName TestServiceImlp
+ * @Description : 测试
+ * @Date 2020/3/26 23:05
+ * @Version 1.0
+ **/
+public class TestServiceImpl implements TestService {
+
+
+}
